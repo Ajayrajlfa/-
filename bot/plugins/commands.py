b025fd53bf3aaebadfe37e056c1b9683eee973bd
Ajayrajlfa <92770467+Ajayrajlfa@users.noreply.github.com>
2021-10-19 09:39:09 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,14 +28,14 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = caption,
+                caption = "@AjayrajLFA",
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'OUR GROUP', url="https://t.me/filmcityraj"
                                 )
                         ]
                     ]
